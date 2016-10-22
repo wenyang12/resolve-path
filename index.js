70ec926aff53fd6a0bc2ad63bcd456e2c0cbf2ec
luoy <luoying@fxiaoke.com>
2016-10-22 15:02:20 +0800

    '兼容windows'

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ module.exports = (content, dirname, root, regExp) => {
   let matchs = getMatchs(content, regExp);
 
   // 当跟目录非以/结尾，强制补上
-  if (!/\/$/.test(root)) root = root + '/';
+  if (!/\/$/.test(root)) root = root + path.sep;
 
   matchs.forEach((match) => {
     let st = match[1];
@@ -31,7 +31,7 @@ module.exports = (content, dirname, root, regExp) => {
       return;
     }
 
-    st = path.resolve(dirname, st).replace(root, '/');
+    st = path.resolve(dirname, st).replace(root, path.sep).replace(/\\/g, '/');
     content = content.replace(match[1], st);
   });
 
